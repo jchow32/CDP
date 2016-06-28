3f531071c9acaa7884fe29e15c3fd589c35d8019
Julie <julie.c.chow@gmail.com>
2016-06-28 12:00:35 -0400

    Update server.R

--- server.R
+++ server.R
@@ -15,75 +15,76 @@ library(shinyjs)
 
 shinyServer(function(input, output, session) {
   observe({
-    toggleState("submit", !is.null(input$email) && input$email != "" && !is.null(input$web_file) && !is.null(input$transc_file))
-  })
-  
-  web_file <- reactive({
-    get(input$web_file)
+    toggleState("submit", !is.null(input$email) && input$email != "" && !is.null(input$web_file) && !is.null(input$transc_file) && input$Theta != "")
   })
   
   observeEvent(input$submit, {
     web_file <- input$web_file
+    pathway <- input$pathway
     transc_file <- input$transc_file
-    info("Thank you!")
-  
-  # observeEvent(input$submit, {
-  #   
-  #   web_file <- input$web_file
-  #   
-  #   output$web_file <- renderText({
-  #     paste(web_file$size)
-  #   })
-
-    transc_file <- input$transc_file
-    # transc.rna = load.gene.data(transc_file, 2) # this is where it breaks
-   # how to determine what column? Numeric??
+    # transc.rna = load.gene.data(transc_file$datapath, 2) # this is where it breaks
+    # how to determine what column? Numeric??
     Theta <- input$Theta
     filter <- input$filter
+    info("Thank you!")
+
+    # output$filter <- renderText({
+    #    paste(filter)
+    # })
 
   if (input$pathway == "Kegg") {
-    kegg.pathways = load.WebGestalt(web_file, 'Kegg') # this is also where is breaks
-    # Warning: Error in readLines: 'con' is not a connection
+    kegg.pathways = load.WebGestalt(web_file$datapath, 'Kegg') 
     KEGGgene.ids = get.genes.kegg(kegg.pathways)
-  #   prioritized.data = list.filter(transc.rna$transposed.data,KEGGgene.ids)
-  #   if (filter == 1) {
-  #     KEGGprioritized.50meanfiltered.data = overall.mean.filter(KEGGprioritized.data, Theta)
-  #   } else {
-  #     KEGGprioritized.50varfiltered.data = overall.mean.filter(KEGGprioritized.data, Theta)
-  #   }
+    # prioritized.data = list.filter(transc.rna$transposed.data,KEGGgene.ids)
+    # if (is.null(filter)) {
+    #   next
+    # } else if (filter == 1) {
+    #   KEGGprioritized.50meanfiltered.data = overall.mean.filter(KEGGprioritized.data, Theta)
+    # } else if (filter == 2) {
+    #   KEGGprioritized.50varfiltered.data = overall.var.filter(KEGGprioritized.data, Theta)
+    # } else {
+    #   KEGGprioritized.50meanfiltered.data = overall.mean.filter(KEGGprioritized.data, Theta)
+    #   KEGGprioritized.50varfiltered.data = overall.var.filter(KEGGprioritized.data, Theta)
+    # }
   }
   else if (input$pathway == "TF") {
-    tf.pathways = load.WebGestalt(web_file, 'TF')
+    tf.pathways = load.WebGestalt(web_file$datapath, 'TF')
     TFgene.ids = get.genes.tf(tf.pathways)
-  #   prioritized.data = list.filter(transc.rna$transposed.data,TFgene.ids)
-  #   if (filter == 1) {
-  #     TFprioritized.50meanfiltered.data = overall.mean.filter(TFprioritized.data, Theta)
-  #   } else {
-  #     TFprioritized.50varfiltered.data = overall.mean.filter(TFprioritized.data, Theta)
-  #   }
+    # prioritized.data = list.filter(transc.rna$transposed.data,TFgene.ids)
+    # if (is.null(filter)) {
+    #   next
+    # } else if (filter == 1) {
+    #   TFprioritized.50meanfiltered.data = overall.mean.filter(TFprioritized.data, Theta)
+    # } else if (fitler == 2) {
+    #   TFprioritized.50varfiltered.data = overall.var.filter(TFprioritized.data, Theta)
+    # } else {
+    #   TFprioritized.50meanfiltered.data = overall.mean.filter(TFprioritized.data, Theta)
+    #   TFprioritized.50varfiltered.data = overall.var.filter(TFprioritized.data, Theta)
+    # }
   }
   else {
-    wiki.pathways = load.WebGestalt(web_file, 'Wiki')
+    wiki.pathways = load.WebGestalt(web_file$datapath, 'Wiki')
     Wikigene.ids = get.genes.wiki(wiki.pathways)
-  #   prioritized.data = list.filter(transc.rna$transposed.data,Wikigene.ids)
-  #   if (filter == 1) {
-  #     Wikiprioritized.50meanfiltered.data = overall.mean.filter(Wikiprioritized.data, Theta)
-  #   } else {
-  #     Wikiprioritized.50varfiltered.data = overall.mean.filter(Wikiprioritized.data, Theta)
-  #   }
+    # prioritized.data = list.filter(transc.rna$transposed.data,Wikigene.ids)
+    # if (is.null(filter)) {
+    #   next
+    # } else if (filter == 1) {
+    #   Wikiprioritized.50meanfiltered.data = overall.mean.filter(Wikiprioritized.data, Theta)
+    # } else if (filter ==2) {
+    #   Wikiprioritized.50varfiltered.data = overall.var.filter(Wikiprioritized.data, Theta)
+    # } else {
+    #   Wikiprioritized.50meanfiltered.data = overall.mean.filter(Wikiprioritized.data, Theta)
+    #   Wikiprioritized.50varfiltered.data = overall.var.filter(Wikiprioritized.data, Theta)
+    # }
   }
 
-  # sendmailOptions(smtpServer="ASPMX.L.GOOGLE.COM")
-  # 
+  # sendmailOptions(smtpServer="ASPMX.L.GOOGLE.COM") 
   # from <- ""
   # to <- ""
   # subject <- ""
   # msg <- c("",
   #          mime_part(final_output, ""))
   # # get the final output
-  # 
   # sendmail(from, to, subject, msg)
   })
 })
-
-
